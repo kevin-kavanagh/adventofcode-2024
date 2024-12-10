67752ed022f73fab5fada02a8dcce6712656a004
Kevin Kavanagh <kevin.kavanagh@thetrainline.com>
2024-12-10 17:32:22 +0000

    Finishing 5

--- Days/Dec5.cs
+++ Days/Dec5.cs
@@ -7,43 +7,73 @@ public class Dec5(ITestOutputHelper output)
     [Fact]
     public async Task Calculate1()
     {
-        var total = 0;
         var (rules, updates) = await GetData();
-
         var after = rules.ToLookup(x => x.Left).ToDictionary(x => x.Key, x => x.Select(y => y.Right).ToHashSet());
         var before = rules.ToLookup(x => x.Right).ToDictionary(x => x.Key, x => x.Select(y => y.Left).ToHashSet());
 
-        foreach(var update in updates)
+        var validUpdates = updates.Where(x => IsValid(x, before, after));
+        var total = validUpdates.Sum(x => x[x.Length / 2]);
+
+        output.WriteLine($"{total}");
+    }
+
+    [Fact]
+    public async Task Calculate2()
+    {
+        var (rules, updates) = await GetData();
+        var after = rules.ToLookup(x => x.Left).ToDictionary(x => x.Key, x => x.Select(y => y.Right).ToHashSet());
+        var before = rules.ToLookup(x => x.Right).ToDictionary(x => x.Key, x => x.Select(y => y.Left).ToHashSet());
+
+        var total = 0;
+        var invalidUpdates = updates.Where(x => !IsValid(x, before, after));
+        foreach (var update in invalidUpdates)
         {
-            bool isvalid = true;
-            for(int i = 0; i < update.Length; i++)
+            var correctedUpdate = new List<int>();
+            var remaining = update.ToList();
+            while (remaining.Count > 0)
             {
-                var num = update[i];
-                var toLeft = update[0..i];
-                var toRight = update[(i + 1)..update.Length];
-
-                var shouldComeAfter = after.TryGetValue(num, out var a) ? a : [];
-                var shouldComeBefore = before.TryGetValue(num, out var b) ? b : [];
-
-                var invalidOnLeft = toLeft.Any(shouldComeAfter.Contains);
-                var invalidOnRight = toRight.Any(shouldComeBefore.Contains);
-
-                if (invalidOnLeft || invalidOnRight)
+                var next = remaining
+                    .FirstOrDefault(x => remaining.Except([x])
+                        .All(y => after.TryGetValue(x, out var others) ? others.Contains(y) : false));
+                if (next == default)
                 {
-                    isvalid = false;
-                    break;
+                    correctedUpdate.AddRange(remaining);
+                    remaining.Clear();
+                }
+                else
+                {
+                    correctedUpdate.Add(next);
+                    remaining.Remove(next);
                 }
             }
 
-            if (isvalid)
-            {
-                var middle = update[update.Length / 2];
-                total += middle;
-            }
-
+            Assert.True(IsValid(correctedUpdate.ToArray(), before, after));
+            total += correctedUpdate[correctedUpdate.Count / 2];
         }
 
         output.WriteLine($"{total}");
+    }
+
+    private static bool IsValid(int[] update, Dictionary<int, HashSet<int>> before, Dictionary<int, HashSet<int>> after)
+    {
+        for (int i = 0; i < update.Length; i++)
+        {
+            var num = update[i];
+            var toLeft = update[0..i];
+            var toRight = update[(i + 1)..update.Length];
+
+            var shouldComeAfter = after.TryGetValue(num, out var a) ? a : [];
+            var shouldComeBefore = before.TryGetValue(num, out var b) ? b : [];
+
+            var invalidOnLeft = toLeft.Any(shouldComeAfter.Contains);
+            var invalidOnRight = toRight.Any(shouldComeBefore.Contains);
+
+            if (invalidOnLeft || invalidOnRight)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     private static async Task<(Rule[] Rules, List<int[]> Updates)> GetData()
