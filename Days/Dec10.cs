a0f38a507e7365417883356af4cc2118713d1f7e
Kevin Kavanagh <kevin.kavanagh@thetrainline.com>
2024-12-15 15:07:49 +0000

    Start 10

---
+++ Days/Dec10.cs
@@ -0,0 +1,75 @@
+﻿using Xunit.Abstractions;
+
+namespace AdventOfCode_2024.Days;
+
+public class Dec10(ITestOutputHelper output)
+{
+    private static readonly char[] Directions = ['N', 'S', 'W', 'E'];
+
+    [Fact]
+    public void Calculate1()
+    {
+        var data = GetData();
+        var locations = data.ToDictionary(x => x.Location);
+
+        var paths = 0;
+        foreach (var tile in data.Where(x => x.Height == 0))
+        {
+            var finalTiles = GetPaths(tile, locations);
+            paths += finalTiles.Distinct().Count();
+        }
+
+        output.WriteLine($"{paths}");
+    }
+
+    private Tile[] GetPaths(Tile tile, Dictionary<Location, Tile> locations)
+    {
+        if (tile.Height == 9)
+        {
+            return [tile];
+        }
+
+        var finalTiles = new List<Tile>();
+        foreach (var direction in Directions)
+        {
+            var nexTile = Get(direction, tile, locations);
+            if (nexTile?.Height == tile.Height + 1)
+            {
+                finalTiles.AddRange(GetPaths(nexTile, locations));
+            }
+        }
+
+        return finalTiles.ToArray();
+    }
+
+    private Tile? Get(char direction, Tile tile, Dictionary<Location, Tile> locations)
+    {
+        return direction switch
+        {
+            'N' => locations.TryGetValue(new Location(tile.Location.X, tile.Location.Y - 1), out var north) ? north : null,
+            'S' => locations.TryGetValue(new Location(tile.Location.X, tile.Location.Y + 1), out var south) ? south : null,
+            'W' => locations.TryGetValue(new Location(tile.Location.X - 1, tile.Location.Y), out var west) ? west : null,
+            'E' => locations.TryGetValue(new Location(tile.Location.X + 1, tile.Location.Y), out var east) ? east : null,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    private Tile[] GetData()
+    {
+        var lines = File.ReadAllLines("./input/Dec10.txt");
+
+        var tiles = new List<Tile>();
+        for (int y = 0; y < lines.Length; y++)
+        {
+            for (int x = 0; x < lines[0].Length; x++)
+            {
+                tiles.Add(new Tile(new Location(x, y), int.TryParse(lines[y][x].ToString(), out var n) ? n : 11));
+            }
+        }
+
+        return tiles.ToArray();
+    }
+
+    private record Location(int X, int Y);
+    private record Tile(Location Location, int Height);
+}
--- Days/Dec9.cs
+++ Days/Dec9.cs
@@ -52,6 +52,70 @@ public class Dec9(ITestOutputHelper output)
         output.WriteLine($"{result}");
     }
 
+    [Fact]
+    public void Calculate2()
+    {
+        var data = GetData();
+
+        var list = new List<Memory>();
+        var id = 0;
+        bool isFile = true;
+        foreach (var digit in data)
+        {
+            if (isFile)
+            {
+                if (digit > 0)
+                {
+                    var blocks = Enumerable.Range(0, digit).Select(_ => new FileBlock(id)).ToArray();
+                    list.Add(new Memory(blocks));
+                }
+                id++;
+            }
+            else
+            {
+                if (digit > 0)
+                {
+                    var empties = Enumerable.Range(0, digit).Select(_ => new Memory([null]));
+                    list.AddRange(empties);
+                }
+            }
+            isFile = !isFile;
+        }
+
+        foreach (var file in list.Skip(1).Where(x => x.Blocks[0] != null).Reverse().ToArray())
+        {
+            var fileIndex = list.IndexOf(file);
+            for (int i = 0; i < fileIndex; i++)
+            {
+                if (list[i].Blocks[0] != null)
+                {
+                    continue;
+                }
+
+                var emptyIndex = i;
+                if(emptyIndex + file.Blocks.Length > list.Count)
+                {
+                    break;
+                }
+
+                var contiguous = list[emptyIndex .. (emptyIndex + file.Blocks.Length)];
+                if(contiguous.All(x => x.Blocks[0] is null))
+                {
+                    list.Remove(file);
+                    list.InsertRange(fileIndex, contiguous);
+                    list.RemoveRange(emptyIndex, contiguous.Count);
+                    list.Insert(emptyIndex, file);
+                    break;
+                }
+            }
+        }
+        var newList = list.SelectMany(x => x.Blocks);
+
+        var result = newList.Select((block, i) => Convert.ToInt64(i) * block?.Id ?? 0).Sum();
+
+        output.WriteLine($"{result}");
+    }
+
     private int[] GetData()
     {
         var nums = File.ReadAllText("./input/Dec9.txt");
@@ -59,4 +123,11 @@ public class Dec9(ITestOutputHelper output)
     }
 
     private record FileBlock(int Id);
+    private record Memory(FileBlock?[] Blocks)
+    {
+        public override string ToString()
+        {
+            return string.Join("", Blocks.Select(x => x?.Id.ToString() ?? "."));
+        }
+    }
 }
